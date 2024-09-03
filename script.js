ff8736df008cd2ec94b5e7177f9f0b2caabd336e
jacopolegovini <legovini96@gmail.com>
2024-09-03 14:59:12 +0200

    exercise completed

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const {createApp} = Vue
 createApp({
     data() {
       return {
-        message: 'Hello World'
+        message: 'Hello World',
+        img: 'https://cdn.britannica.com/34/233234-050-1649BFA9/Pug-dog.jpg'
       }
     }
   }).mount('#app')
